329887d9a87cc382ae7d6e6e3fad826b28913dbf
kevinniel <k.niel.pro@gmail.com>
2025-03-27 11:34:13 +0100

    add exo 1

---
+++ javascript_bases/exercice1.js
@@ -0,0 +1,12 @@
+// Crée une variable `prenom` contenant ton prénom
+// Crée une variable `age` contenant ton âge
+// Affiche les deux dans la console sous la forme : "Je m'appelle [prenom] et j'ai [age] ans"
+
+let prenom = "Alice";
+let age = 25;
+
+// concaténation = on additionne des chaînes de caractères
+console.log("Je m'appelle " + prenom + " et j'ai " + age + " ans");
+
+// en utilisant des "backtits" avec des variables dedans
+console.log(`Je m'appelle ${prenom} et j'ai ${age} ans`);
