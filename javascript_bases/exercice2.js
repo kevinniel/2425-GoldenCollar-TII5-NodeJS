97c6e4f9e24753169a05927a0fea0baaceee98fe
kevinniel <k.niel.pro@gmail.com>
2025-03-27 11:47:54 +0100

    exo 2

---
+++ javascript_bases/exercice2.js
@@ -0,0 +1,13 @@
+// Crée deux variables a et b (nombres)
+// Calcule leur somme, leur différence et leur produit
+// Affiche chaque résultat dans la console avec une phrase explicite
+// Exemple : "La somme de a et b est : ..."
+
+let a = 5;
+let b = 3;
+let somme = a + b;
+let difference = a - b;
+let produit = a * b;
+console.log(`La somme de ${a} et ${b} est : ${somme}`);
+console.log(`La différence de ${a} et ${b} est : ${difference}`);
+console.log(`Le produit de ${a} et ${b} est : ${produit}`);
