3242f4e21615e07ee151d3a2fa35109912e42240
kevinniel <k.niel.pro@gmail.com>
2025-03-27 12:02:11 +0100

    update

---
+++ javascript_bases/exercice3.js
@@ -0,0 +1,9 @@
+// Crée un tableau appelé fruits contenant 3 fruits différents
+// Affiche le premier fruit
+// Ajoute un fruit à la fin du tableau
+// Affiche le nouveau tableau
+
+let fruits = ["pomme", "banane", "fraise"];
+console.log(fruits[0]);
+fruits.push("orange");
+console.log(fruits);
