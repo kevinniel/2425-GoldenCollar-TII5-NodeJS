fcd0d4f3da79982d0b2f1b76d82d831920c3eb7c
kevinniel <k.niel.pro@gmail.com>
2025-04-04 10:18:05 +0200

    update

--- javascript_bases/dab.js
+++ javascript_bases/dab.js
@@ -1,8 +1,41 @@
 import promptSync from 'prompt-sync';
 const prompt = promptSync();
 
+const stock = {
+    coupures: {
+        500: 0,
+        200: 10,
+        100: 10,
+        50: 10,
+        20: 10,
+        10: 10,
+        5: 10,
+        2: 10,
+        1: 10,
+    },
+
+    retirer: function(montant) {
+        let rendu = {};
+        Object.keys(this.coupures)
+            .map(Number)
+            .sort((a, b) => b - a)
+            .forEach(valeur => {
+                console.log(`Coupure de ${valeur} : ${this.coupures[valeur]} billets`);
+                if(montant >= valeur) {
+                    rendu[valeur] = Math.floor(montant / valeur);
+                    if (rendu[valeur] > this.coupures[valeur]) {
+                        rendu[valeur] = this.coupures[valeur];
+                    }
+                    montant -= rendu[valeur] * valeur;
+                    this.coupures[valeur] -= rendu[valeur];
+                }
+            });
+        return rendu;
+    },
+}
+
 const compteBancaire = {
-    solde: 0,
+    solde: 100000,
 
     consulterSolde: function() {
         console.log(`Votre solde est de ${this.solde} euros.`);
@@ -22,6 +55,7 @@ const compteBancaire = {
         montant = parseInt(montant);
         if (montant > 0 && montant <= this.solde) {
             this.solde -= montant;
+            stock.retirer(montant);
             console.log(`Vous avez retiré ${montant} euros.`);
         } else if (montant > this.solde) {
             console.log("Fonds insuffisants.");
@@ -33,7 +67,6 @@ const compteBancaire = {
 }
 
 const dab = {
-
     // initialisation
     initialiser: function() {
         console.clear();
@@ -45,6 +78,7 @@ const dab = {
     // menu
     afficherMenu: function() {
         console.log("==== MENU DU DAB ====");
+        console.log("0. Consulter le stock");
         console.log("1. Consulter le solde");
         console.log("2. Déposer de l'argent");
         console.log("3. Retirer de l'argent");
@@ -54,7 +88,11 @@ const dab = {
     // actions du menu
     choisirOption: function() {
         let option = prompt();
+        console.clear();
         switch (option) {
+            case "0":
+                this.consulterStock();
+                break;
             case "1":
                 this.consulterCompte();
                 break;
@@ -74,6 +112,13 @@ const dab = {
         }
         this.afficherMenu();
         this.choisirOption();
+    },
+    
+    consulterStock: function() {
+        console.log("==== STOCK ====");
+        Object.keys(stock.coupures).forEach(coupure => {
+            console.log(`Coupure de ${coupure} : ${stock.coupures[coupure]} billets`);
+        });
     },
 
     consulterCompte: function() {
